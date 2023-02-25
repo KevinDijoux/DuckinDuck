99b3f18bb63c50993e0d3757b0108e089a8b171d
Kevin <kevindijoux01@gmail.com>
2023-02-25 10:25:02 +0100

    FIXE Selector

--- DuckinDuck/Assets/Scripts/Misc/Selector.cs
+++ DuckinDuck/Assets/Scripts/Misc/Selector.cs
@@ -1,64 +1,64 @@
-using UnityEngine;
-using UnityEngine.EventSystems;
+//using UnityEngine;
+//using UnityEngine.EventSystems;
 
-//Made by Melinon Remy
-public class Selector : MonoBehaviour
-{
-    public GameObject towerHUD;
-    public GameObject usineHUD;
-    [SerializeField] LayerMask interactibleLayer;
-    //private HUDwhenSelect openHUDref;
-    private bool isMouseOnUI;
-    [SerializeField] private Transform anchor = null;
-    //[SerializeField] private HUDAnimationController hudAnimationController;
-    public bool IsMouseOnUI => isMouseOnUI;
+////Made by Melinon Remy
+//public class Selector : MonoBehaviour
+//{
+//    public GameObject towerHUD;
+//    public GameObject usineHUD;
+//    [SerializeField] LayerMask interactibleLayer;
+//    //private HUDwhenSelect openHUDref;
+//    private bool isMouseOnUI;
+//    [SerializeField] private Transform anchor = null;
+//    //[SerializeField] private HUDAnimationController hudAnimationController;
+//    public bool IsMouseOnUI => isMouseOnUI;
 
-    public void Select()
-    {
-        RaycastToOpenHUDRef();
-    }
+//    public void Select()
+//    {
+//        RaycastToOpenHUDRef();
+//    }
 
-    private void RaycastToOpenHUDRef()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, float.MaxValue, interactibleLayer) && !isMouseOnUI)
-        {
-            if (openHUDref != null)
-            {
-                openHUDref.OnDeselect();
-                //openHUDref.DisconnectToEvents();
-                openHUDref = null;
-            }
-            if (hit.transform.gameObject.GetComponent<HUDwhenSelect>() != null)
-            {
-                openHUDref = hit.transform.gameObject.GetComponent<HUDwhenSelect>();
-                openHUDref.SetAnchor(anchor);
-                openHUDref.OnSelect();
-            }
-        }
-        else if (openHUDref != null && !isMouseOnUI)
-        {
-            openHUDref.OnDeselect();
-            openHUDref = null;
-        }
-    }
+//    private void RaycastToOpenHUDRef()
+//    {
+//        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//        RaycastHit hit;
+//        if (Physics.Raycast(ray, out hit, float.MaxValue, interactibleLayer) && !isMouseOnUI)
+//        {
+//            if (openHUDref != null)
+//            {
+//                openHUDref.OnDeselect();
+//                //openHUDref.DisconnectToEvents();
+//                openHUDref = null;
+//            }
+//            if (hit.transform.gameObject.GetComponent<HUDwhenSelect>() != null)
+//            {
+//                openHUDref = hit.transform.gameObject.GetComponent<HUDwhenSelect>();
+//                openHUDref.SetAnchor(anchor);
+//                openHUDref.OnSelect();
+//            }
+//        }
+//        else if (openHUDref != null && !isMouseOnUI)
+//        {
+//            openHUDref.OnDeselect();
+//            openHUDref = null;
+//        }
+//    }
 
-    //Check if mouse is on UI
-    private void Update()
-    {
-        if (EventSystem.current.IsPointerOverGameObject(PointerInputModule.kMouseLeftId))
-        {
-            isMouseOnUI = true;
-        }
-        else
-        {
-            isMouseOnUI = false;
-        }
-    }
+//    //Check if mouse is on UI
+//    private void Update()
+//    {
+//        if (EventSystem.current.IsPointerOverGameObject(PointerInputModule.kMouseLeftId))
+//        {
+//            isMouseOnUI = true;
+//        }
+//        else
+//        {
+//            isMouseOnUI = false;
+//        }
+//    }
 
-    public void SwapHUD()
-    {
-        hudAnimationController.HUDswaper();
-    }
-}
+//    public void SwapHUD()
+//    {
+//        hudAnimationController.HUDswaper();
+//    }
+//}
