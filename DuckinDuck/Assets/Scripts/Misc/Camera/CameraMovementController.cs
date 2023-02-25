081ef643a46127091b2ef017fccd052b9c24eaa8
Tykoph <45920212+Tykoph@users.noreply.github.com>
2023-02-25 02:29:12 +0100

    ADD Ducks and auberge

--- DuckinDuck/Assets/Scripts/Misc/Camera/CameraMovementController.cs
+++ DuckinDuck/Assets/Scripts/Misc/Camera/CameraMovementController.cs
@@ -1,18 +1,40 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class CameraMovementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField]
+	private float _cameraSpeed = 25f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField]
+	private List<GameObject> _movementLimits = new List<GameObject>();
+
+	private GameObject _movementLimit;
+
+
+	private void OnEnable()
+	{
+		_movementLimit = _movementLimits[0];
+	}
+
+	private	void GoodOlMovement()
+	{
+		transform.localPosition = new UnityEngine.Vector3
+									(Mathf.Clamp(transform.localPosition.x + _cameraSpeed * Input.GetAxis("Horizontal") * Time.deltaTime,
+									-_movementLimit.transform.localPosition.x, _movementLimit.transform.position.x),
+
+									transform.position.y,
+
+									Mathf.Clamp(transform.localPosition.z + _cameraSpeed * Input.GetAxis("Vertical") * Time.deltaTime,
+									-_movementLimit.transform.localPosition.z, _movementLimit.transform.localPosition.z));
+
+	}
+
+	void LateUpdate()
+	{
+		GoodOlMovement();
+	}
 }
