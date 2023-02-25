faa6a2ae89e3a9eae545b3cc02ab7c91bcee6e91
Kevin <kevindijoux01@gmail.com>
2023-02-25 10:23:51 +0100

    ADD Selector Scripts

--- DuckinDuck/Assets/Scripts/Misc/Selector.cs
+++ DuckinDuck/Assets/Scripts/Misc/Selector.cs
@@ -1,18 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
+//Made by Melinon Remy
 public class Selector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject towerHUD;
+    public GameObject usineHUD;
+    [SerializeField] LayerMask interactibleLayer;
+    //private HUDwhenSelect openHUDref;
+    private bool isMouseOnUI;
+    [SerializeField] private Transform anchor = null;
+    //[SerializeField] private HUDAnimationController hudAnimationController;
+    public bool IsMouseOnUI => isMouseOnUI;
+
+    public void Select()
     {
-        
+        RaycastToOpenHUDRef();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RaycastToOpenHUDRef()
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, float.MaxValue, interactibleLayer) && !isMouseOnUI)
+        {
+            if (openHUDref != null)
+            {
+                openHUDref.OnDeselect();
+                //openHUDref.DisconnectToEvents();
+                openHUDref = null;
+            }
+            if (hit.transform.gameObject.GetComponent<HUDwhenSelect>() != null)
+            {
+                openHUDref = hit.transform.gameObject.GetComponent<HUDwhenSelect>();
+                openHUDref.SetAnchor(anchor);
+                openHUDref.OnSelect();
+            }
+        }
+        else if (openHUDref != null && !isMouseOnUI)
+        {
+            openHUDref.OnDeselect();
+            openHUDref = null;
+        }
+    }
+
+    //Check if mouse is on UI
+    private void Update()
+    {
+        if (EventSystem.current.IsPointerOverGameObject(PointerInputModule.kMouseLeftId))
+        {
+            isMouseOnUI = true;
+        }
+        else
+        {
+            isMouseOnUI = false;
+        }
+    }
+
+    public void SwapHUD()
+    {
+        hudAnimationController.HUDswaper();
     }
 }
